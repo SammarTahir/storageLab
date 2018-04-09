3b9ab5f667fb492a300d1b7d3d2b8f561128b1c8
SammarTahir <G00347526@gmit.ie>
2018-04-09 16:36:44 +0100

    third commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UpdateStatusPage } from '../update-status/update-status';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  openUpdateStatusPage(){
+   this.navCtrl.push(UpdateStatusPage);   
   }
 
 }
