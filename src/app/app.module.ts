08898a22912e19d440ccf3a73e50e2ea7d09b002
SammarTahir <G00347526@gmit.ie>
2018-04-09 16:29:38 +0100

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { UpdateStatusPage } from '../pages/update-status/update-status';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    UpdateStatusPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    UpdateStatusPage
   ],
   providers: [
     StatusBar,
