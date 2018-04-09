a4246c64d0a2b0874bdbdd1bc2c84bb0dc0e0d01
SammarTahir <G00347526@gmit.ie>
2018-04-09 16:58:32 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicStorageModule } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { UpdateStatusPage } from '../pages/update-status/update-status';
@@ -16,7 +16,8 @@ import { UpdateStatusPage } from '../pages/update-status/update-status';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/update-status/update-status.ts
+++ src/pages/update-status/update-status.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
 /**
  * Generated class for the UpdateStatusPage page.
  *
@@ -14,12 +14,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'update-status.html',
 })
 export class UpdateStatusPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  myStatus:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UpdateStatusPage');
   }
-
+  saveUpdate(){
+    console.log(this.myStatus);
+  }
 }
